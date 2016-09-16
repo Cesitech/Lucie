b6cad5825cd01dfc1624ecf43b0e684d7d18099e
GTorreil <guillaumetorreilles@MacBook-Air-de-Guillaume.local>
2016-09-16 20:02:12 +0200

    Catch network errors
    Catch network errors and prevent the lib’s network-related methods from
    crashing.

--- Code/LucieTimeLib/LucieTimeLib.py
+++ Code/LucieTimeLib/LucieTimeLib.py
@@ -110,19 +110,28 @@ def get_from_network():
     http://just-the-time.appspot.com renvoie une string sous la forme : 2016-05-31 12:45:05
     Attention, l'heure renvoyée est calée sur GMT !
     """
-    response = urllib2.urlopen('http://just-the-time.appspot.com')
-    string = response.read()
-    string = string.split(" ") # Là ça donne une liste : [date,time]
-    time = string[1].split(":")
-    h=time[0]
-    m=time[1]
-    s=time[2]
+    err = 0
+    try:
+        response = urllib2.urlopen('http://just-the-time.appspot.com')
+        
+    except urllib2.URLError:
+        # On n'a pas internet !! c'est la merde! 
+        print("Impossible de se connecter au serveur horaire, l'heure exacte n'a pas été récupérée.")
+        err = 1
+        return 0
+        
+    if err==0 : 
+        string = response.read()
+        string = string.split(" ") # Là ça donne une liste : [date,time]
+        time = string[1].split(":")
+        h=time[0]
+        m=time[1]
+        s=time[2]
     
-    
-    d = 0
-    mo = 0
-    y = 0
-    return [d,mo,y,h,m,s]
+        d = 0
+        mo = 0
+        y = 0
+        return [d,mo,y,h,m,s]
 
 def test_lib():
     """
@@ -171,7 +180,7 @@ def test_lib():
 
     print("Lecture de l'heure sur le réseau...")
     get_from_network()
-    print("L'heure récupéré sur le réseau est " + str(config.gmtdiff))
+    print("L'heure récupéré sur le réseau est " + str(config.gmtdiff)) # HUM!! ça c'est de la merde pure !!
 
 
 ###  T E S T  ###
